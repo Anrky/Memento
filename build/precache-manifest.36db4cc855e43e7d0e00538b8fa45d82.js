6a43b52dd6aa81895e69e79e095991b9932fe74b
Rishi Salunke <63459370+Anrky@users.noreply.github.com>
2021-05-18 16:50:23 +0530

    Add files via upload

---
+++ build/precache-manifest.36db4cc855e43e7d0e00538b8fa45d82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaa7d7a021b0e85a6e18ac8093cfc50e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e2541d57aac22551ca6",
+    "url": "/static/css/main.08177b78.chunk.css"
+  },
+  {
+    "revision": "6bfdc4776029d4435080",
+    "url": "/static/js/2.890f412a.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.890f412a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e2541d57aac22551ca6",
+    "url": "/static/js/main.35e87806.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  }
+]);
